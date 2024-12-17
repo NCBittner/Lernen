f3d70c5426b12723fead242e88cdc413b9c44a05
ncb3101 <nicolecbittner@gmail.com>
2024-12-17 16:42:24 +0100

    Menu Update

---
+++ public/menu.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  /* Hamburger move */
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  /* Panel move */
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+  }
+
+  /* Sidebar */
+  function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+});
